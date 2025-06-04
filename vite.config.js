406764e172cb9bf01c05c2aff5af4fd06a5c2db3
Noufel <noufel_seif_el_islam.nasri@univ-constantine2.dz>
2025-06-04 16:10:01 +0100

    fixx

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import process from 'process'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:process.env.VITE_ENV_PATH || "/My_Portfolio/",
+  base:process.env.VITE_BASE_PATH || "/My_Portfolio",
 })
